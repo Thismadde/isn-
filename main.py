5fef1c9f4df1e0d7cc579b6af30f00156761799f
Robert Dixon <guiguikiwa@yahoo.fr>
2019-04-13 14:46:40 +0200

    Gravité a ameliorer mais fonctionne !!!

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-""""""""""""""""""""""""" FONCTION DE SAUT EN COURS, GRAVITE FONCTIONNEL ??''''''''''''''''''''
-"""""""""""""""""""""""""
-
-
 import pygame
 import time
 pygame.init()
@@ -11,7 +7,7 @@ pygame.init()
 HEIGTH_display = 768
 WIDTH_display = 1280
 x = 50
-y = 768-3*64
+y = 768-6*64
 width = 50
 height = 60
 vel = 2
@@ -69,17 +65,6 @@ ciel_sprites = pygame.sprite.Group()
 myfont = pygame.font.SysFont("monospace",30)
 ''''''''''''''''''''
 
-
-#def Updating_After_Player(x,y):
-#    map.draw()
-"""
-    x_ancien = round(x/TILESIZE)*TILESIZE
-    y_ancien = round(y/TILESIZE)*TILESIZE
-    for i in range(-2,3):
-        for z in range(-2,3):
-            if(x_ancien+(i*TILESIZE),y_ancien+z*TILESIZE) in ciel:
-                win.blit(blue_img,((x_ancien+i*TILESIZE),y_ancien+z*TILESIZE))
-"""
 class Camera:
     def __init__(self,width,height):
         self.camera = pygame.Rect(0,0,width,height)
@@ -116,7 +101,7 @@ class Player(pygame.sprite.Sprite):
         self.orientation = "Right"
         self.isCollinding = True
         self.isJumping = False
-        self.jumpCount = 10
+        self.jumpCount = 50
         self.vies = 3
         self.health = 100
         self.collision_with_ground = True
@@ -133,46 +118,29 @@ class Player(pygame.sprite.Sprite):
         textfont = myfont.render("X"+str(self.vies),3,RED)
         win.blit(textfont,(400,5))      
     def gravity(self):
-        dx = 0
+        refresh = 0
         self.collision_with_ground = False
-        while not self.collision_with_ground:
+        if not self.collision_with_ground:
+            self.rect.y += 2
             blocks_hit_list = pygame.sprite.spritecollide(self,sol_sprites,False)
             if not(blocks_hit_list == []):
                 self.collision_with_ground = True
-                self.rect.y -= 1
-                map.draw()
-                player.draw_player()
+                self.rect.y -= 2
                 return True 
             else:
-                self.rect.y += 1
-                dx += 1 
-                self.collision_with_ground = False   
-                if (dx > 20):
-                    map.draw()
-                    player.draw_player()
-
+                map.draw()
+                player.draw_player()
                 
-                   
-    def collision_while_jumping(self,negative):
-        if(self.isJumping):
-            self.rect.y -= self.jumpCount ** 2 * 0.5 * negative
-            blocks_hit_list = pygame.sprite.spritecollide(self,sol_sprites,False)
-            if not(blocks_hit_list == []):
-                self.rect.y += (self.jumpCount ** 2 * 0.5 * negative)*2
-                return True
-            else:
-                self.rect.y += (self.jumpCount ** 2 * 0.5 * negative)
-                return False
 
     def collision_with_walls(self):
         if self.orientation == "Right":
-            self.rect.x += vel
+            #self.rect.x += vel
             blocks_hit_list = pygame.sprite.spritecollide(self,sol_sprites,False)
             if not(blocks_hit_list == []):
-                self.rect.x -= vel*2
+                self.rect.x -= vel
                 return True
             else:
-                self.rect.x -= vel
+                #self.rect.x -= vel
                 return False
         if self.orientation == "Left":
             self.rect.x -= vel
@@ -216,16 +184,28 @@ class Player(pygame.sprite.Sprite):
         if (self.orientation == "Down"):
             x_new = camera.apply_player([self.rect.x])
             win.blit(mario_up,(x_new,self.rect.y))
-<<<<<<< HEAD
-      
-    def moove(self,keys):     
-        if(self.gravity()):
-=======
+    def jump(self):
+        print(self.jumpCount)
+        if self.jumpCount >= -50:
+            neg = 1
+            if self.jumpCount < 0:
+                neg = -1
+            self.rect.y -= self.jumpCount**2 * 0.005 * neg
+            blocks_hit_list = pygame.sprite.spritecollide(self,sol_sprites,False)
+            self.jumpCount -= 1
+            if not (blocks_hit_list == []):
+                self.jumpCount = 50
+                self.isJumping = False
+                self.rect.y += self.jumpCount**2 * 0.005 * neg
+        else:
+            self.isJumping = False
+            self.jumpCount = 50
+        map.draw()
+        self.draw_player()
 
-    def moove(self,keys):
-        #self.isCollindingWithGround()
-        #if(self.gravity()):
->>>>>>> a5d3507af260987894518d03d83c4aff9609b945
+    def moove(self,keys):  
+        if self.isJumping:
+            self.jump()
             if keys[pygame.K_LEFT]:
                 self.orientation = "Left"
                 if not(self.x - vel<0) and not self.collision_with_walls():
@@ -240,7 +220,23 @@ class Player(pygame.sprite.Sprite):
                     self.rect.x += vel
                     camera.update(player)
                     self.draw_player()
-            if not (self.isJumping):
+
+        if(not self.isJumping):
+            if keys[pygame.K_LEFT]:
+                self.orientation = "Left"
+                if not(self.x - vel<0) and not self.collision_with_walls():
+                    map.draw()
+                    self.rect.x -= vel
+                    camera.update(player)
+                    self.draw_player()
+            if keys[pygame.K_RIGHT]:
+                self.orientation = "Right"
+                if not self.collision_with_walls():
+                    map.draw()
+                    self.rect.x += vel
+                    camera.update(player)
+                    self.draw_player()
+            if ((not self.isJumping) and (self.gravity())):
                 if keys[pygame.K_DOWN]:
                     self.orientation = "Down"
                     if not ((self.y+vel)>HEIGTH_display-height)and not self.collision_with_walls():
@@ -249,37 +245,7 @@ class Player(pygame.sprite.Sprite):
                         camera.update(player)
                         self.draw_player()
                 if keys[pygame.K_UP]:
-                    self.orientation = "Up"
-                    if not ((self.y-vel)<0)and not self.collision_with_walls():
-                        map.draw()
-                        self.rect.y -= vel
-                        camera.update(player)
-                        self.draw_player()
-                #self.draw_player()
-                #if keys[pygame.K_SPACE]:
-                #   self.isJumping = True
-            """if (self.isJumping): Fonction de saut a ameliorer
-                if self.jumpCount >= -10:
-                    negative = 1
-                    self.orientation = "Up"
-                    if self.jumpCount < 1:
-                        self.orientation = "Down"
-                        negative = -1
-                    if not self.collision_while_jumping(negative):
-                    #if not ((y - jumpCount ** 2 * 0.1) < 0):
-                    # y -= jumpCount*0.01 * 2 * 0.5
-                        Updating_After_Player(self.rect.x,self.rect.y)
-                        self.rect.y -= self.jumpCount ** 2 * 0.5 * negative
-                        self.draw_player()
-                        self.jumpCount -= 1
-                    #print("Jump " + str(jumpCount))
-                    #else :
-                    #   jumpCount = 10
-                    #  isJumping = False
-                else:
-                    self.isJumping = False
-                    self.jumpCount = 10
-                """
+                    self.isJumping = True
             return x,y
 
 class Sol(pygame.sprite.Sprite):
@@ -313,7 +279,6 @@ class Surprise(pygame.sprite.Sprite):
 
 class Map(pygame.sprite.Sprite):
     def __init__(self,WIDTH_display,HEIGHT_display,First_Load):
-        z = 5
         self.width = WIDTH_display
         self.height = HEIGHT_display
         self.load = First_Load
@@ -406,24 +371,16 @@ USEREVENT = 24
 pygame.time.set_timer(USEREVENT, 1000)
 fps_all = 0
 number = 0
-#font = ImageFont.truetype('Roboto-Bold.ttf', size=4)
-'''def draw_vie():
-    pygame.draw.rect(win,RED,[0,0,150,50],2)
-    win.blit(mario_vie,(2,5))
-    draw.text((42,35/2), "x"+self.vies, font=font )
-'''
 while run:
     clock = pygame.time.Clock()
     milliseconds = clock.tick(FPS)
-    #if (time_sleep > -500):
-     #   time_sleep -= -1
-    #else:
-     #   time_sleep = 500
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             print("Moyenne des FPS :" + str(fps_all/number))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.isJumping = True
         #Compteur de FPS
         elif event.type == USEREVENT:
             fps_label = font_cambria.render('FPS : {:.2f}'.format(timer.get_fps()), True, RED)
@@ -437,7 +394,6 @@ while run:
     win.blit(blue_img,(0,0))
     win.blit(blue_img,(TILESIZE,0))
     win.blit(fps_label,fps_rect)
-    #draw_vie()
     #Fin du compteur
     pygame.display.update()
 
